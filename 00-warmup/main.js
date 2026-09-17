2ceb99070fef994f80e3bc56557f43bd11c2a419
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-09-17 19:37:58 -0400

    post-class

--- 00-warmup/main.js
+++ 00-warmup/main.js
@@ -14,14 +14,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const weatherRes = await fetch(weatherUrl);
     const weatherData = await weatherRes.json();
 
-    console.log(weatherData);
+    // console.log(weatherData);
 
     // TODO: combine temperatures and times into dataset (rows/columns)
+    const times = weatherData.hourly.time;
+    const temps = weatherData.hourly.temperature_2m;
+
+    const data = [];
+    for (let idx = 0; idx < times.length; idx = idx + 1) {
+      data.push({
+        time: times[idx],
+        temp: temps[idx],
+      });
+    }
+    // console.log(data);
+
+    // clear svg in case we get a click after an initial graph has been drawn
+    document.querySelector("#weather-svg").innerHTML = "";
 
     const svgWidth = document.querySelector("#weather-svg").clientWidth;
-    const svgHeight = parseInt(0.66 * svgWidth);
-    const leftMargin = 20;
-    const bottomMargin = 40;
+    const svgHeight = parseInt(0.5 * svgWidth);
+    const leftMargin = 24;
+    const bottomMargin = 24;
     const plotWidth = svgWidth - leftMargin;
     const plotHeight = svgHeight - bottomMargin;
 
@@ -31,11 +45,50 @@ document.addEventListener("DOMContentLoaded", () => {
       .attr("height", svgHeight);
 
     // TODO: xScale (scalePoint)
+    const xScale = d3.scalePoint()
+      .domain(times)
+      .range([leftMargin, svgWidth]);
+
     // TODO: yScale (scaleLinear)
+    const yScale = d3.scaleLinear()
+      .domain([d3.min(data, d => d.temp), d3.max(data, d => d.temp)])
+      .range([plotHeight, 0]);
+
+    // scale temp values to colors between blue and red
+    const fScale = d3.scaleSequential()
+      .domain([d3.min(data, d => d.temp), d3.max(data, d => d.temp)])
+      .range(["blue", "red"]);
 
     // TODO: bind and draw data
+    svg.append("g")
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("cx", d => xScale(d.time))
+      .attr("cy", d => yScale(d.temp))
+      .attr("r", 2)
+      .attr("fill", d => fScale(d.temp));
+
+    // TODO: y-axis
+    //       create a new empty group
+    //       move this group to the right so there's room to the left of the line to display numbers
+    //       call the axisLeft function
+    //       this creates the actual svg elements for the axis,
+    //            based on values given to yScale during its creation (domain/range),
+    //            and applies it to the group g
+    svg.append("g")
+      .attr("transform", `translate(${leftMargin}, 0)`)
+      .call(d3.axisLeft(yScale));
 
     // TODO: x-axis
-    // TODO: y-axis
+    //       similar to above, but here we manually have to filter the times values so only a subset of them get used as ticks on the axis
+    //       and we're formatting the strings to only show the month/day parts of the full timestamp
+    svg.append("g")
+      .attr("transform", `translate(0, ${plotHeight})`)
+      .call(
+        d3.axisBottom(xScale)
+          .tickValues(times.filter((t, i) => i % 48 == 0))
+          .tickFormat(d => d.slice(5,10))
+      );
   });
 });
